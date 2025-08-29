98c11debcec28d32943ae3142f9bbbf837d8c0d2
fisayo-dev <olufisayobadina@gmail.com>
2025-08-29 19:19:31 +0100

    chore: remove github link in header

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,7 +7,6 @@ const Header = () => {
         <div className="app-container flex justify-between items-center h-[72px]">
             <h2 className="font-bold text-xl">Smeal</h2>
             <div className="flex items-center gap-4">
-                <Link href="#">Features</Link>
                 <Link href="https://github.com/fisayo-dev/seo-analyzer" target="_blank">Github</Link>
                 <Link href="/login">
                   <Button>Get started</Button>
