9ab6482abbe57cee78ca7928d9dd06c6a6e5a95d
fisayo-dev <olufisayobadina@gmail.com>
2025-09-03 20:33:42 +0100

    style: create border

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -13,7 +13,7 @@ import Link from "next/link"
 
 const LoginForm = () =>  {
   return (
-   <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+   <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
         <Link href="/" className="flex items-center gap-2 self-center font-medium">
           <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
@@ -22,7 +22,7 @@ const LoginForm = () =>  {
           Smeal.
         </Link>
         <div className={cn("flex flex-col gap-6")}>
-          <Card>
+          <Card className="shadow-neutral-50">
             <CardHeader className="text-center">
               <CardTitle className="text-xl">Welcome to Smeal.</CardTitle>
               <CardDescription>
