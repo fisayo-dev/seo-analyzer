9f0f9e7ad90e1599426716de06e610981e8bbe08
fisayo-dev <olufisayobadina@gmail.com>
2025-09-04 22:30:20 +0100

    feat: add meta data

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,12 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import { Metadata } from "next";
+
+
+export const metadata: Metadata = {
+  title: "Smeal - SEO Analyzer",
+  description: "Analyze and optimize your website's SEO performance with Smeal, the ultimate SEO analyzer tool.",
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
