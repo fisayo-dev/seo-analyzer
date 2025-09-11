2bd4817f61206297f7c561456b53c2617c7d2323
fisayo-dev <olufisayobadina@gmail.com>
2025-09-11 21:29:09 +0100

    feat(schema): upafe id colun in seo_analysis table to default random value

---
+++ lib/db/migrations/0002_special_vermin.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "seo_analysis" ALTER COLUMN "on_page" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "seo_analysis" ALTER COLUMN "content" DROP NOT NULL;--> statement-breakpoint
+ALTER TABLE "seo_analysis" ALTER COLUMN "technical" DROP NOT NULL;
