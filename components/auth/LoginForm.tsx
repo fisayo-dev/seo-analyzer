0e59c257e575a088e89afa9209405abfa6ead598
fisayo-dev <olufisayobadina@gmail.com>
2025-09-17 17:29:33 +0100

    feat: add logo to home and login page

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -8,9 +8,10 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
-import { GalleryVerticalEnd, GithubIcon } from "lucide-react"
+import { GithubIcon } from "lucide-react"
 import Link from "next/link"
 import { signInWithGitHub, signInWithGoogle } from "@/lib/auth/client"
+import Image from "next/image"
 
 const LoginForm = () =>  {
     const handleGoogleSignIn = async () =>{
@@ -32,10 +33,10 @@ const LoginForm = () =>  {
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
             <Link href="/" className="flex items-center gap-2 self-center font-medium">
-            <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-                <GalleryVerticalEnd className="size-4" />
-            </div>
-            Scanzie.
+                <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                    <Image src="/favicon.png" alt="Logo" height={24} width={24}/>
+                </div>
+                <span className="text-xl">Scanzie</span>
             </Link>
             <div className={cn("flex flex-col gap-6")}>
             <Card className="shadow-neutral-50">
