196faecec6144340aec4f939d628c2e02ebf22c4
fisayo-dev <olufisayobadina@gmail.com>
2025-09-19 00:24:46 +0100

    style: remove bg color in image

--- components/auth/LoginForm.tsx
+++ components/auth/LoginForm.tsx
@@ -41,7 +41,7 @@ const LoginForm = () =>  {
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
         <div className="flex w-full max-w-sm flex-col gap-6">
             <Link href="/" className="flex items-center gap-2 self-center font-medium">
-                <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
+                <div className="flex size-6 items-center justify-center rounded-md">
                     <Image src="/favicon.png" alt="Logo" height={24} width={24}/>
                 </div>
                 <span className="text-xl">Scanzie</span>
